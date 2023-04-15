4d9e4dbd32b8c3dbbdf4fdfc709ff7857c0404a7
ArtemHordiienko <s1mpleworkpost@gmail.com>
2023-04-15 03:30:35 +0300

    add logic to create messagas, filter, repository, postgres and application.properties

---
+++ src/main/java/com/example/twitter/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.example.twitter.repository;
+
+import com.example.twitter.model.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
