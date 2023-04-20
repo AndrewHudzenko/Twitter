f6238b034b211c5e0a5e8273ba2aa10c2d4fc250
ArtemHordiienko <s1mpleworkpost@gmail.com>
2023-04-20 19:43:14 +0300

    add syntax fix

--- src/main/java/com/example/twitter/controller/MainController.java
+++ src/main/java/com/example/twitter/controller/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages = messageService.findAll();
+        Iterable<Message> messages;
 
         if (filter != null && !filter.isEmpty()) {
             messages = messageService.findByTag(filter);
