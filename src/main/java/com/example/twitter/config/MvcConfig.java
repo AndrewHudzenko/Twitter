b318995768b0f0f9252b8a33a451437fc52012e4
ArtemHordiienko <s1mpleworkpost@gmail.com>
2023-05-08 14:33:54 +0300

    change mail password in prop/ add aws sdk dep to pom/ add twitter blue bird logo to our page/ add s3 config/ transfer logic for adding messages from controller to service/ replace local storage of images with s3/ delete uploads folder with previous images

--- src/main/java/com/example/twitter/config/MvcConfig.java
+++ src/main/java/com/example/twitter/config/MvcConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Value("${upload.path}")
-    private String uploadPath;
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
@@ -18,7 +18,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
-                .addResourceLocations("file://" + uploadPath + "/");
+                .addResourceLocations("https://" + bucketName + ".s3.amazonaws.com/");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
--- src/main/java/com/example/twitter/service/MessageServiceImpl.java
+++ src/main/java/com/example/twitter/service/MessageServiceImpl.java
@@ -1,15 +1,29 @@
 package com.example.twitter.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.example.twitter.model.Message;
+import com.example.twitter.model.User;
 import com.example.twitter.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MessageServiceImpl implements MessageService {
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+
+    private final AmazonS3 amazonS3;
     private final MessageRepository messageRepository;
 
-    public MessageServiceImpl(MessageRepository messageRepository) {
+    public MessageServiceImpl(AmazonS3 amazonS3, MessageRepository messageRepository) {
+        this.amazonS3 = amazonS3;
         this.messageRepository = messageRepository;
     }
 
@@ -26,5 +40,48 @@ public class MessageServiceImpl implements MessageService {
     @Override
     public List<Message> findByTag(String tag) {
         return messageRepository.findByTag(tag);
+    }
+
+    @Override
+    public Message add(String text, String tag, User user, MultipartFile file) {
+        Message message = Message.builder()
+                .text(text)
+                .tag(tag)
+                .author(user)
+                .build();
+
+        if (!file.isEmpty() && !file.getOriginalFilename().isEmpty()) {
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            PutObjectRequest putObjectRequest = null;
+            try {
+                putObjectRequest = new PutObjectRequest(bucketName, resultFilename, file.getInputStream(), metadata);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            amazonS3.putObject(putObjectRequest);
+
+            message.setFilename(resultFilename);
+        }
+
+        return message;
+
+//        if (!file.isEmpty() && !file.getOriginalFilename().isEmpty()) {
+//            File uploadDir = new File(uploadPath);
+//
+//            if (!uploadDir.exists()) {
+//                uploadDir.mkdir();
+//            }
+//            String uuidFile = UUID.randomUUID().toString();
+//            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+//
+//            file.transferTo(new File(uploadPath + "/" + resultFilename));
+//
+//            message.setFilename(resultFilename);
+//        }
     }
 }
